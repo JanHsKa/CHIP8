154ce01389658c2147e91c68e5b5e1992a97924b
JanHsKa <jancmalle@gmail.com>
2021-03-07 17:08:08 +0100

    Removed build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/janmalle/Studium/Semester5/Projektarbeit/Chip8
-# Build directory: /home/janmalle/Studium/Semester5/Projektarbeit/Chip8/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib")
-subdirs("main")
--- build/main/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/janmalle/Studium/Semester5/Projektarbeit/Chip8/main
-# Build directory: /home/janmalle/Studium/Semester5/Projektarbeit/Chip8/build/main
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
